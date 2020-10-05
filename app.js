523edc5f05589e0093848dd2a494669c632f3629
kal9isa <docalisa_it@yahoo.com>
2020-10-05 17:57:20 +0330

    setup server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const express = require("express");
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.status(200).send("Gello!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on 3000");
+});
---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: "Myapp",
+      script: "app.js",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "debian",
+      host: "192.168.1.185",
+      ref: "origin/master",
+      repo: "https://github.com/Kal9Isa/test.git",
+      path: "/home/debian/projects/fota/",
+      ssh_options: ["ForwardAgent=yes", "StrictHostKeyChecking=no"],
+      "pre-setup": "npm install -g pm2@latest",
+      "pre-deploy-local": "echo 'This is a local executed command'",
+      "post-deploy": "npm install && pm2 start",
+    },
+  },
+};
