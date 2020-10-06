1d71129fa9f8dc42c63941997136583b28ca2c4f
kal9isa <docalisa_it@yahoo.com>
2020-10-06 13:00:14 +0330

    update server

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.status(200).send("Gello!");
+app.get("/api/update", (req, res) => {
+  res.status(200).send("1.19");
 });
 
 app.listen(3000, () => {
